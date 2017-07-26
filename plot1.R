fdc4d890f620dfa43ca2561bfca63c674ea60e71
Ahmed3sam <ahmed3sam@gmail.com>
2017-07-26 07:55:23 +0200

    Codes and PNG

---
+++ plot1.R
@@ -0,0 +1,18 @@
+
+if(!file.exists("C:/Users/Ahmed/Documents/R/data")){dir.create("C:/Users/Ahmed/Documents/R/data")}
+fileurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileurl, destfile = "C:/Users/Ahmed/Documents/R/data/household_power_consumption.zip")
+
+## Unzip Data
+unzip("C:/Users/Ahmed/Documents/R/data/household_power_consumption.zip", exdir = "C:/Users/Ahmed/Documents/R/data")
+
+#read data
+data_full <- read.table("C:/Users/Ahmed/Documents/R/data/household_power_consumption.txt", header=TRUE, na.strings="?", sep=";")
+data <- data_full[(data_full$Date=="1/2/2007" | data_full$Date=="2/2/2007" ), ]
+
+## Plot 1
+hist(data$Global_active_power, main="Global Active Power", xlab="Global Active Power (kilowatts)", ylab="Frequency", col="Red", cex.sub=0.8)
+
+## export file
+dev.copy(png, file="plot1.png", height=480, width=480)
+dev.off()
---
+++ plot2.R
@@ -0,0 +1,20 @@
+if(!file.exists("C:/Users/Ahmed/Documents/R/data")){dir.create("C:/Users/Ahmed/Documents/R/data")}
+fileurl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileurl, destfile = "C:/Users/Ahmed/Documents/R/data/household_power_consumption.zip")
+
+## Unzip Data
+unzip("C:/Users/Ahmed/Documents/R/data/household_power_consumption.zip", exdir = "C:/Users/Ahmed/Documents/R/data")
+
+#read data
+data_full <- read.table("C:/Users/Ahmed/Documents/R/data/household_power_consumption.txt", header=TRUE, na.strings="?", sep=";")
+data <- data_full[(data_full$Date=="1/2/2007" | data_full$Date=="2/2/2007" ), ]
+
+#format date and time
+data$Date <- as.Date(data$Date, format="%d/%m/%Y")
+dateTime <- paste(data$Date, data$Time)
+data$DateTime <- as.POSIXct(dateTime)
+
+#plot2
+plot(data$Global_active_power~data$DateTime, type="l", ylab="Global Active Power (kilowatts)", xlab="")
+dev.copy(png, file="plot2.png", height=480, width=480)
+dev.off()
